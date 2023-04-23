8e6e0951d34851106696d1b208ef6b3c3ba36dfd
Houthacker <houthacker@houthacker.io>
2023-04-23 15:51:51 +0200

    Add merge tests

--- Google_Tests/noid/storage/BPlusTreeTests.cpp
+++ Google_Tests/noid/storage/BPlusTreeTests.cpp
@@ -71,7 +71,10 @@ TEST_F(BPlusTreeFixture, Redistribution) {
   std::stringstream buf;
   tree->Write(buf);
 
-  auto expected_after_setup = "[17]\n[12 15] [19 21 23 25]\n[2* 5*] [12* 13*] [15* 16*] [17* 18*] [19* 20*] [21* 22*] [23* 24*] [25* 26* 27* 29*]\n";
+  auto expected_after_setup =
+      "[17]\n"
+      "[12 15] [19 21 23 25]\n"
+      "[2* 5*] [12* 13*] [15* 16*] [17* 18*] [19* 20*] [21* 22*] [23* 24*] [25* 26* 27* 29*]\n";
   EXPECT_STREQ(buf.str().c_str(), expected_after_setup);
 
   auto remove_key = key_base;
@@ -84,6 +87,49 @@ TEST_F(BPlusTreeFixture, Redistribution) {
   buf = std::stringstream();
   tree->Write(buf);
 
-  auto expected_after_redistribution = "[17]\n[12 15] [19 21 23 27]\n[2* 5*] [12* 13*] [15* 16*] [17* 18*] [19* 20*] [21* 22*] [23* 26*] [27* 29*]\n";
+  auto expected_after_redistribution =
+      "[17]\n"
+      "[12 15] [19 21 23 27]\n"
+      "[2* 5*] [12* 13*] [15* 16*] [17* 18*] [19* 20*] [21* 22*] [23* 26*] [27* 29*]\n";
   EXPECT_STREQ(buf.str().c_str(), expected_after_redistribution) << "Expect redistribution of [23* 24*] [25* 26* 27* 29*] -> [23* 26*] [27* 29*] after removal of 24*";
+}
+
+TEST_F(BPlusTreeFixture, Merger) {
+  K key_base = {57, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  V value = {1, 3, 3, 7};
+
+  std::vector<byte> key_values = {2, 5, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29};
+  for (auto b : key_values) {
+    K key = key_base;
+    key[BTREE_KEY_SIZE - 1] = b;
+
+    tree->Insert(key, value);
+  }
+
+  std::stringstream buf;
+  tree->Write(buf);
+
+  auto expected_after_setup =
+      "[17]\n"
+      "[12 15] [19 21 23 25]\n"
+      "[2* 5*] [12* 13*] [15* 16*] [17* 18*] [19* 20*] [21* 22*] [23* 24*] [25* 26* 27* 29*]\n";
+  EXPECT_STREQ(buf.str().c_str(), expected_after_setup);
+
+  // Removing key [..., 20] must merge [17* 18*] with [19* <removed>*], since this tree tries to merge with left
+  // siblings first. This also causes the removal of key 19 from their parent node [19 21 23 25].
+  auto remove_key = key_base;
+  remove_key[BTREE_KEY_SIZE - 1] = 20;
+  auto removed = tree->Remove(remove_key);
+
+  EXPECT_TRUE(removed.has_value()) << "Expect a removed value";
+  EXPECT_THAT(removed.value(), ContainerEq(value));
+
+  buf = std::stringstream();
+  tree->Write(buf);
+
+  auto expected_after_merge =
+      "[17]\n"
+      "[12 15] [21 23 25]\n"
+      "[2* 5*] [12* 13*] [15* 16*] [17* 18* 19*] [21* 22*] [23* 24*] [25* 26* 27* 29*]\n";
+  EXPECT_STREQ(buf.str().c_str(), expected_after_merge) << "Expect merge to leave tree in valid state";
 }
